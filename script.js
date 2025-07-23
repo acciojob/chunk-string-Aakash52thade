5bc782728fca834f049e684840166d6c0a9dc181
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-23 23:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
+function chunkString(str, size) {
+  if (str === null) return [];
 
-
-function stringChop(str, size){
-
-	if(size <= 0) return;
-
-	let result = [];
-
-	for(let i=0; i<str.length; i+=size){
-		result.push(str.slice(i, i+size));
-	}
-	return result;
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  return result;
 }
 
+// Do not change the code below
 const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+const size = parseInt(prompt("Enter Chunk Size."));
+alert(JSON.stringify(chunkString(str, size)));
